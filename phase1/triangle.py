62884a3e81adc218be3393390d3129e587caecec
Dalla936 <dalla.diallo@edu.univ-paris13.fr>
2026-02-28 16:09:17 +0100

    add fichiers

---
+++ phase1/triangle.py
@@ -0,0 +1,9 @@
+from main import Point
+
+class Triangle():
+
+    def __init__(self, Point1 : Point, Point2 : Point, Point3 : Point):
+        self._point1 = Point1
+        self._point2 = Point2
+        self._point3 = Point3
+    
