9cf742bb2c66e64454a52d836c8793bb1090991f
Dalla936 <dalla.diallo@edu.univ-paris13.fr>
2026-02-28 15:55:06 +0100

    push des modifcations pour la suite avec la creation de triangle, segment, diagramme etc et refactor

--- phase1/main.py
+++ phase1/main.py
@@ -1,5 +1,6 @@
 from math import sqrt
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Point:
 
@@ -44,12 +45,46 @@ class Point:
          point_A = Point(liste_points[i][0], liste_points[i][1]) #PointA
          point_B = Point(liste_points[i+1][0], liste_points[i+1][1]) #PointB
          point_C = Point(liste_points[i+2][0], liste_points[i+2][1]) #PointC
+         point_D = Point(liste_points[i+3][0], liste_points[i+3][1]) #PointD
          x_mediatrice_AB, y_mediatrice_AB = point_A.definir_mediatrice(point_B)
          mediatrice_AB = Point(x_mediatrice_AB,y_mediatrice_AB)
          x_mediatrice_BC, y_mediatrice_BC = point_B.definir_mediatrice(point_C)
          mediatrice_BC = Point(x_mediatrice_BC,y_mediatrice_BC)
          x_mediatrice_AC, y_mediatrice_AC = point_A.definir_mediatrice(point_C)
          mediatrice_AC = Point(x_mediatrice_AC,y_mediatrice_AC)
+         x_mediatrice_AD, y_mediatrice_AD = point_A.definir_mediatrice(point_D)
+         mediatrice_AD = Point(x_mediatrice_AD,y_mediatrice_AD)
+         x_mediatrice_BD, y_mediatrice_BD = point_B.definir_mediatrice(point_D)
+         mediatrice_BD = Point(x_mediatrice_BD,y_mediatrice_BD)
+         x_mediatrice_DC, y_mediatrice_DC = point_D.definir_mediatrice(point_C)
+         mediatrice_DC = Point(x_mediatrice_DC,y_mediatrice_DC)
+    
+        
+         pente_perpendiculaire_AC = point_A.calculer_pente_perpendiculaire(point_C)
+         pente_perpendiculaire_AB = point_A.calculer_pente_perpendiculaire(point_B)
+         print(f"pente perpendiculaire AB = {pente_perpendiculaire_AB}")
+         pente_perpendiculaire_BC = point_B.calculer_pente_perpendiculaire(point_C)
+         pente_perpendiculaire_AD = point_A.calculer_pente_perpendiculaire(point_D)
+         pente_perpendiculaire_BD = point_B.calculer_pente_perpendiculaire(point_D)
+         pente_perpendiculaire_DC = point_D.calculer_pente_perpendiculaire(point_C)
+
+         resultats = {}
+         resultats["point_A"] = point_A
+         resultats["point_B"] = point_B
+         resultats["point_C"] = point_C
+         resultats["point_D"] = point_D
+         resultats["mediatrice_AB"] = mediatrice_AB
+         resultats["mediatrice_BC"] = mediatrice_BC
+         resultats["mediatrice_AC"] = mediatrice_AC
+         resultats["mediatrice_AD"] = mediatrice_AD
+         resultats["mediatrice_BD"] = mediatrice_BD
+         resultats["mediatrice_DC"] = mediatrice_DC
+         resultats["pente_perpendiculaire_DC"] = pente_perpendiculaire_DC
+         resultats["pente_perpendiculaire_AC"] = pente_perpendiculaire_AC
+         resultats["pente_perpendiculaire_AB"] = pente_perpendiculaire_AB
+         resultats["pente_perpendiculaire_BC"] = pente_perpendiculaire_BC
+         resultats["pente_perpendiculaire_AD"] = pente_perpendiculaire_AD
+         resultats["pente_perpendiculaire_BD"] = pente_perpendiculaire_BD
 
 
          print(f" Les coordonnées de la médiatrice_AB sont {mediatrice_AB.x, mediatrice_AB.y} , et celle de la mediatrice_BC sont {mediatrice_BC.x,mediatrice_BC.y}")
@@ -57,6 +92,10 @@ class Point:
          print(f"Coordonnées du point B = {point_B.x, point_B.y}")
          print(f"Coordonnées du point C = {point_C.x, point_C.y}")
          print(f"coordonnées mediatrice A_C = {mediatrice_AC.x, mediatrice_AC.y}")
+         print(f"coordonnées mediatrice D_C = {mediatrice_DC.x, mediatrice_DC.y}")
+        
+
+         return resultats
          
          
 
@@ -79,11 +118,29 @@ class Point:
     @y.setter
     def y(self, value):
         self._y = value
+    def calculer_coefficients_mediatrice(self,autre_point):
+        a = 2 * (autre_point.x - self._x)
+        b = 2 * (autre_point.y - self._y)
+        c = self._x**2 + self._y**2 - autre_point.x**2 - autre_point.y**2
+        return a, b, c
+    
+    def calculer_pente_perpendiculaire(self, autre_point):
+        dx = autre_point.x - self._x
+        dy = autre_point.y - self._y
+        if dy == 0:
+            return None  
+        pente_perpendiculaire = -dx / dy  # = -1/pente
+        return pente_perpendiculaire
+    
+    def __str__(self):
+        return f"Point({self._x}, {self._y})"
+    def dessiner():
+        fig = plt.figure()
+        plt.show()
+
 
 if __name__ == "__main__":
-        point1 = Point(2, 4, "Point1")
-        mediat_x, mediat_y = point1.definir_mediatrice(Point(5.3, 4.5, "Point2"))
-        print(f"Mediatrice: ({mediat_x}, {mediat_y})")
+        point1 = Point(0, 0, "Point1")
         point1.determiner_triangle_while()
 
-    
+
